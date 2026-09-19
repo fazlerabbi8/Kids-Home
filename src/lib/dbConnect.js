6bae318de8fc76e75bd8d004bdb127b2cc360dc1
fazlerabbi13 <mohammad0132794@gmail.com>
2026-09-19 19:34:16 +0600

    change favicon.

--- src/lib/dbConnect.js
+++ src/lib/dbConnect.js
@@ -1,21 +1,21 @@
 import dns from "dns";
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = process.env.DB_URI;
 const dbname = process.env.DB_NAME;
 export const collections = {
-    PRODUCTS: "products",
-}
+  PRODUCTS: "products",
+};
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
-export const dbConnect =(collectionName) =>{
-    return client.db(dbname).collection(collectionName);
-}
+export const dbConnect = (collectionName) => {
+  return client.db(dbname).collection(collectionName);
+};
