444c38f5139b8a108e36b7d42f98b329ac87edf9
fazlerabbi13 <mohammad0132794@gmail.com>
2026-09-20 12:22:35 +0600

    added loading and not-found page.

---
+++ src/app/loading.jsx
@@ -0,0 +1,10 @@
+
+const loading = () => {
+    return (
+        <div className="text-center mt-20">
+            <span className="loading loading-spinner text-primary"></span>
+        </div>
+    );
+};
+
+export default loading;
