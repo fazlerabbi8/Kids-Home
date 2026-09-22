3c8f2b2e6e48fad46f3630fe3f0e80e1644028c5
fazlerabbi13 <mohammad0132794@gmail.com>
2026-09-22 11:31:04 +0600

    fixed active routes.

--- src/components/ActiveButton/NavLink.jsx
+++ src/components/ActiveButton/NavLink.jsx
@@ -1,15 +1,23 @@
 "use client"
+
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 const NavLink = ({ href, children }) => {
   const path = usePathname();
-  return  <Link
-      className={`${path.startsWith(href) && "text-primary"} font-medium`}
+
+  const isActive = href === "/" 
+    ? path === "/" 
+    : path.startsWith(href);
+
+  return (
+    <Link
+      className={`${isActive ? "text-primary" : ""} font-medium`}
       href={href}
     >
       {children}
     </Link>
+  );
 };
 
 export default NavLink;
